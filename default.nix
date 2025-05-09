56ce4651cfed7056929272e46f6fabb9f0d9d340
Asa Mimir <dawsfinklea@gmail.com>
2025-05-08 21:47:22 -0500

    tried to copy from AUR PKGBUILD

--- default.nix
+++ default.nix
@@ -1,23 +1,10 @@
-{
-  lib,
-  stdenv,
-  fetchFromGitHub,
-  xfce,
-  bash,
-  cmake,
-  coreutils,
-  fakeroot,
-  gcc,
-  gnumake,
-  pkg-config,
-  python312Full,
-  ...
-}:
+with import <nixpkgs> { };
 stdenv.mkDerivation rec {
   pname = "xfce-winxp-tc";
   version = "45a78d3";
 
-  buildInputs = [
+  buildInputs = with pkgs; [
+    git
     bash
     cmake
     coreutils
@@ -26,14 +13,47 @@ stdenv.mkDerivation rec {
     gnumake
     pkg-config
     python312Full
+    xorg.xcursorgen
+    xfce.garcon
+    sass
+    libpulseaudio
+    glib
+    webkitgtk_4_1
+    libzip
+    upower
+    networkmanager
+    libcanberra
+    lightdm
   ];
+  
+  src = ./.;
 
-  src = fetchFromGitHub {
-    owner = "rozniak";
-    repo = "xfce-winxp-tc";
-    rev = "${version}";
-    hash = "";
-  };
+  buildPhase = ''
+  cd $src/packaging
+  CFLAGS = '-w' ./buildall.sh -z
+  '';
+
+  installPhase = ''
+  # Package shared libraries
+
+  cd $src/packaging/build/shared
+  lib_list=$(find . -mindepth 1 -maxdepth 1 -type d)
+	for lib in $lib_list; do
+		cd "$lib"
+		make DESTDIR="$out/" install
+		cd -
+	done
+
+  # Package components
+  cd $src/packaging/build
+	comp_list=$(sed 's/#.*$//g' < "../targets")
+	for comp in $comp_list; do
+		cd "$comp"
+		make DESTDIR="$out/" install
+		cd -
+	done
+  '';
+
 
   
 }
