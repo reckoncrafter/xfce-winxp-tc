917816866e04b049676c3df74aa618405c5cbcf8
Asa Mimir <dawsfinklea@gmail.com>
2025-05-09 17:13:53 -0500

    fixed shebangs and directory changes

--- default.nix
+++ default.nix
@@ -28,15 +28,26 @@ stdenv.mkDerivation rec {
   
   src = ./.;
 
+  dontConfigure = true;
+  CFLAGS = "-w";
+
+  preBuild = ''
+  substituteInPlace packaging/*.sh \
+  --replace-warn '/usr/bin/env bash' ${bash}/bin/bash
+  '';
+
   buildPhase = ''
-  cd $src/packaging
-  CFLAGS = '-w' ./buildall.sh -z
+  runHook preBuild
+  cd packaging
+  ./buildall.sh -z
+  runHook postBuild
   '';
 
   installPhase = ''
+  runHook preInstall
   # Package shared libraries
-
-  cd $src/packaging/build/shared
+  cd /build/xfce-winxp-tc
+  cd packaging/build/shared
   lib_list=$(find . -mindepth 1 -maxdepth 1 -type d)
 	for lib in $lib_list; do
 		cd "$lib"
@@ -45,13 +56,15 @@ stdenv.mkDerivation rec {
 	done
 
   # Package components
-  cd $src/packaging/build
+  cd /build/xfce-winxp-tc
+  cd packaging/build
 	comp_list=$(sed 's/#.*$//g' < "../targets")
 	for comp in $comp_list; do
 		cd "$comp"
 		make DESTDIR="$out/" install
 		cd -
 	done
+  runHook postInstall
   '';
 
 
